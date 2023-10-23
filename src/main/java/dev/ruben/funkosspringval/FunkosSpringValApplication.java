23c914c23a6c2d2c669e240c1232bac916b20219
Rubenoide03 <rubenfernandezp03@gmail.com>
2023-10-23 19:53:14 +0200

    Initial commit

---
+++ src/main/java/dev/ruben/funkosspringval/FunkosSpringValApplication.java
@@ -0,0 +1,13 @@
+package dev.ruben.funkosspringval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunkosSpringValApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunkosSpringValApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ruben/funkosspringval/FunkosSpringValApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ruben.funkosspringval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunkosSpringValApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
